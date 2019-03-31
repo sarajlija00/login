cd53a7fb40362aacb56721d629afd86db9b910b3
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-04-01 00:22:13 +0200

    corrected function send

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function login (){
-    let xhttp = new XMLHttpRequest;
+    let xhttp = new XMLHttpRequest ();
     let obj = {};
     obj.email = document.getElementById ('email').value;
     obj.password = document.getElementById ('password').value;
@@ -25,16 +25,21 @@ function login (){
 let token;
 
 function send (){
-    xhttp = new XMLHttpRequest;
+xhttp = new XMLHttpRequest ();
     xhttp.open ("GET", 'https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/user/profile', true);
     xhttp.setRequestHeader ('Authorization', token.token);
     xhttp.send ();
     xhttp.onreadystatechange = function (){
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             console.log(xhttp.responseText);
-            document.getElementById ('rezultat').innerHTML = xhttp.responseText;
+            // document.getElementById ('rezultat').innerHTML = xhttp.responseText;
             let responseObject = JSON.parse (xhttp.responseText);
-            document.getElementById ('response').innerHTML = responseObject.responseText;
+            document.getElementById ('response').innerHTML = `
+                ID: ${responseObject.id} <br>
+                First name: ${responseObject.first_name} <br>
+                Last name: ${responseObject.last_name} <br>
+                E-mail: ${responseObject.email} <br>
+            `;
             
         }
     }
